6d797a96653ddff6076885fb2a9d5ad095179e5f
Alexander K <jonhy.blaze@gmail.com>
2022-10-26 21:24:28 +0300

    changing precahing revision for index.html

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox
 
 
 workbox.precaching.precacheAndRoute([
-  {url: '/index.html', revision: null},
+  {url: '/index.html', revision: '199203'},
   {url: '/style.css', revision: null},
   {url: '/reset.css', revision: null},
   {url: '/script.js', revision: null},
